1b159321a843d34729c618c2c9678d7797481b9f
shivambhanu <shivambhanu6924@gmail.com>
2023-12-16 07:01:58 +0530

    Created 2 APIs for menu-items and booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('menu-items', views.MenuItemView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    
+    path('booking', views.BookingView.as_view()),
+    path('booking/<int:pk>', views.BookingSingleView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import MenuItemSerializer, BookingSerializer
+from .models import Menu, Booking
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+#views for menu-items
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+
+
+#views for booking a table
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class BookingSingleView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
