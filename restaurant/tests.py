38eb5ee21f1bcb2943afa7cf126b7609f9719131
shivambhanu <shivambhanu6924@gmail.com>
2023-12-17 19:04:40 +0530

    Did unit testing on models

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Menu
 
-# Create your tests here.
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title='Cake', price=49.99, inventory=10)
+        self.assertEqual(str(item), 'Cake : 49.99')
